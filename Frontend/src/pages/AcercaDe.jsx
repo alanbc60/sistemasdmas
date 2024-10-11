ba6acb9832cc3ea40c0316be85b27d62bd10afd5
AlanUrielMS <116377472+AlanUrielMS@users.noreply.github.com>
2024-10-11 10:54:37 -0600

    Update AcercaDe.jsx
    subrayado de elementos en hover

--- Frontend/src/pages/AcercaDe.jsx
+++ Frontend/src/pages/AcercaDe.jsx
@@ -19,16 +19,19 @@ export default function AcercaDe() {
 
     return (
         <div id='acercade-conteiner' className='flex flex-wrap items-center justify-center gap-[5%] pb-[1.75rem]'>
-        <ul id='acercade-nav' className='list-none p-0 m-0 flex flex-col gap-[1.25rem]'>{siteManagement.map((e,index)=>(
-                    <li key={`${index}-nav`} className={`cursor-pointer px-3 text-center h-6 ${activeIndex===index? 'active': ''}`}>
-                        <button onClick={()=>handleClickSiteManagementMenu(index)}>
+        <ul id='acercade-nav' className='list-none p-0 m-0 flex flex-col gap-[1.25rem]  '>{siteManagement.map((e,index)=>(
+                    <li key={`${index}-nav`} className={`cursor-pointer px-3 text-center  h-6 ${activeIndex===index? 'active': ''}`}>
+                        <button onClick={()=>handleClickSiteManagementMenu(index)} className='min-w-[150px] hover:text-orange-500 nav-link relative after:absolute after:w-full after:h-0.5 after:bg-orange-500 after:bottom-0 after:left-0 after:scale-x-0 hover:after:scale-x-100 after:transition-transform'>
+                        
                         {e.title}
+                        
+                        
                         </button>
                     </li>
                     ))}
         </ul>
-        <ul id='acercade-members'>{currentSiteManagement.members.map((e,index)=>(
-            <li key={`${index}-member`} className='list-none p-4 m-0 flex flex-col gap-2 border border-dashed border-orange-uam border-l-0 border-r-0'>{e}</li>
+        <ul id='acercade-members' className='border border-dashed border-orange-uam border-l-0 border-r-0' >{currentSiteManagement.members.map((e,index)=>(
+            <li key={`${index}-member`} className='list-none p-4 m-0 flex flex-col gap-2 '>{e}</li>
         ))}</ul>
         </div>
     )
