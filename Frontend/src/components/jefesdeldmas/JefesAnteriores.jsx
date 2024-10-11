ad0bc5a01dfd5df31fc1a5a02e3858f60d5ad1fa
AlanUrielMS <116377472+AlanUrielMS@users.noreply.github.com>
2024-10-10 23:08:29 -0600

    Update JefesAnteriores.jsx
    Ajuste para pantallas pequeñas así como ajuste de columnas para el div contenedor de los jefes anteriores.

--- Frontend/src/components/jefesdeldmas/JefesAnteriores.jsx
+++ Frontend/src/components/jefesdeldmas/JefesAnteriores.jsx
@@ -13,17 +13,19 @@ import PropTypes from 'prop-types';
 export default function JefesAnteriores({updateLightboxItem}) {
     const column1Jefes = jefesdeldmas.slice(1, Math.floor(jefesdeldmas.length / 2) + 1);
     const column2Jefes = jefesdeldmas.slice(Math.floor(jefesdeldmas.length / 2) + 1);
+    const columna = 'grid place-item-center grid-rows-1/3 gap-4 relative';
+
     return(
-        <article id='jefes-anteriores-container'>
+        <article id='jefes-anteriores-container' className="">
             <h3 className='jefe-title'>Jefaturas Anteriores del DMAS</h3>
-            <div id='jefes-anteriores-content'>
-                <div className='columna'>
+            <div id='jefes-anteriores-content' className="grid md:grid-cols-[1fr_1.1fr]  gap-4">
+                <div id='columna' className={`${columna} `} >
                     {column1Jefes.map((jefe, index) => (
                         <JefeItem jefe={jefe} key={`${index}-col1`} updateLightboxItem={updateLightboxItem}/>
                     ))}
                     <div className='column-gold'></div>
                 </div>
-                <div className='columna'>
+                <div id='columna' className={`${columna} `} >
                     {column2Jefes.map((jefe, index) => (
                         <JefeItem jefe={jefe} key={`${index}-col2`} updateLightboxItem={updateLightboxItem}/>
                     ))}
