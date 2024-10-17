725e8550546d44d1d901f624229fd2e1d8650e67
AlanUrielMS <116377472+AlanUrielMS@users.noreply.github.com>
2024-10-16 23:03:09 -0600

    Update Lightbox.jsx

--- Frontend/src/elements/Lightbox.jsx
+++ Frontend/src/elements/Lightbox.jsx
@@ -4,12 +4,12 @@ import { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const lightboxStyle = 'fixed inset-0 z-10 w-full h-full';
-const lightboxJefe = 'absolute p-[50px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[800px] max-h-[675px] mx-auto text-center rounded-lg border-[4px] border-orange-uam bg-white/90 backdrop-blur-sm overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-white';
-const closIcon = 'cursor-pointer transition-colors duration-300 ease-in-out hover:text-orange-500';
+const lightboxPrincipal = 'fixed inset-0 z-10 w-full h-full';
+const lightboxStyle = 'absolute p-[50px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[800px] max-h-[675px] mx-auto text-center rounded-[25px] border-[4px] border-orange-uam bg-white/90 backdrop-blur-sm overflow-y-auto scrollbar-size-[10px] scrollbar-thumb-gray-300 scrollbar-track-blue-200';
+const closeIcon = 'cursor-pointer transition-colors duration-300 ease-in-out hover:text-orange-500';
 
 
-export function LightboxJefe({ lightboxItem, updateState }) {
+export function LightboxImagen({ lightboxItem, updateState }) {
 
     useEffect(() => {
         const cerrarConEsc = (event) => {
@@ -25,19 +25,20 @@ export function LightboxJefe({ lightboxItem, updateState }) {
 
 
     return (
-        <section id='lightbox poup-jefe' className={`${lightboxStyle} ${lightboxJefe}`} style={{ display: lightboxItem ? 'block' : 'none' }}>
+        <section id='lightbox poup-jefe' className={`${lightboxPrincipal} ${lightboxStyle}`} style={{ display: lightboxItem ? 'block' : 'none' }}>
             {lightboxItem &&
                 <article className='grid place-item-center grid-cols-1'>
                     <div className='flex justify-end'>
                         <FontAwesomeIcon id='close-icon'
-                            className={`${closIcon}`}
+                            className={`${closeIcon}`}
                             icon={faTimes}
                             onClick={() => updateState(null)} />
                     </div>
-                    {lightboxItem.nombre && <h3>{lightboxItem.nombre}</h3>}
+                    {lightboxItem.nombre && <h3 className='text-[20px] font-bold'>{lightboxItem.nombre}</h3>}
                     {lightboxItem.period && <p>{lightboxItem.period}</p>}
                     <div className='flex justify-center py-3'>
                         <img
+                            className='rounded-[50px] border-[4px] border-orange-uam'
                             src={lightboxItem.imgSrc}
                             alt={`photo of ${lightboxItem.imgSrc}`} />
                     </div>
@@ -48,35 +49,9 @@ export function LightboxJefe({ lightboxItem, updateState }) {
         </section>
     )
 }
-LightboxJefe.propTypes = {
+
+LightboxImagen.propTypes = {
     lightboxItem: PropTypes.object,
     updateState: PropTypes.func.isRequired
 }
 
-export function LightboxImg({ imgSrc, updateState }) {
-    useEffect(() => {
-        const cerrarConEsc = (event) => {
-            if (event.code === 'Escape')
-                updateState(null)
-        };
-        document.addEventListener('keydown', cerrarConEsc)
-        return () => {
-            document.removeEventListener('keydown', cerrarConEsc)
-        }
-    }, [updateState])
-    return (
-        <section id='popup-img' className={`${lightboxStyle}`} style={{ display: imgSrc ? 'block' : 'none' }}>
-            <figure>
-
-                <FontAwesomeIcon icon={faTimes} onClick={() => updateState(null)} />
-                {imgSrc &&
-                    <img src={imgSrc} alt='Sin descripción' />
-                }
-            </figure>
-        </section>
-    )
-}
-LightboxImg.propTypes = {
-    imgSrc: PropTypes.string,
-    updateState: PropTypes.func.isRequired
-}
